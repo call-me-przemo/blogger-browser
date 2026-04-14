cef6f214274d52a68f66444a75714df9a1e84155
Przemysław Mielniczek <pmiel13@gmail.com>
2026-04-14 20:45:58 +0200

    feat: add shadcn

--- src/pages/root/index.tsx
+++ src/pages/root/index.tsx
@@ -1,8 +1,15 @@
+import { Button } from "@/components/ui/button";
+import { useImmer } from "use-immer";
+
 export function RootPage() {
+  const [count, setCount] = useImmer(0);
+
   return (
-    <main>
-      <h1>Very nice</h1>
-      <p>I have a chair DX</p>
+    <main className="p-4 flex gap-4 items-center bg-stone-700">
+      <Button className="cursor-pointer" onClick={() => setCount((c) => ++c)}>
+        Click me !!!
+      </Button>
+      <p className="text-yellow-600">You have clicked button {count} times</p>
     </main>
   );
 }
